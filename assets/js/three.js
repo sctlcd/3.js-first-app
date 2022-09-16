20ea1b2872b16fb10c33256a1dff305dd9d9c86a
lucinda <lucinda.souchet@gmail.com>
2022-09-16 13:46:05 +0200

    resizing window

--- assets/js/three.js
+++ assets/js/three.js
@@ -26,3 +26,9 @@ function animate() {
 };
 
 animate();
+
+window.addEventListener('resize', () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+})
