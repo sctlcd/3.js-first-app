cbc8fee05d583c3728f8c3648f5d851f42d920be
lucinda <lucinda.souchet@gmail.com>
2022-09-18 17:13:12 +0200

    add spheres

--- assets/js/three.js
+++ assets/js/three.js
@@ -45,7 +45,17 @@ boxMesh.geometry.colorsNeedUpdate = true;
 boxMesh.rotation.set(40, 0, 40);
 scene.add(boxMesh);
 
-// Lights
+// create spheres
+const sphereMeshes = [];
+const sphereGeometry = new THREE.SphereGeometry(0.1, 32, 32);
+const sphereMaterial = new THREE.MeshLambertMaterial({color: 0xC56CEF});
+for (let i=0; i<4; i++) {
+  sphereMeshes[i] = new THREE.Mesh(sphereGeometry, sphereMaterial);
+  sphereMeshes[i].position.set(0, 0, 0);
+  scene.add(sphereMeshes[i]);
+}
+
+// lights
 const lights = [];
 const lightHelpers = [];
 const lightValues = [
@@ -70,31 +80,61 @@ for (let i=0; i<6; i++) {
   scene.add(lights[i]);
   console.log(lights[i].color);
 
-  lightHelpers[i] = new THREE.PointLightHelper(lights[i], 0.7);
+  // light Helpers
+  lightHelpers[i] = new THREE.PointLightHelper(lights[i], 0.6);
   scene.add(lightHelpers[i]);
+
+  // axes Helper
+  const axesHelper = new THREE.AxesHelper(5);
+  scene.add(axesHelper); // X == red, Y == green, Z == blue
 }
 
-//Trackball Controls for Camera 
+// trackball Controls for Camera 
 const controls = new TrackballControls(camera, renderer.domElement); 
 controls.rotateSpeed = 4;
 controls.dynamicDampingFactor = 0.15;
 
-// Make canvas responsive
+// make canvas responsive
 window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 })
 
+// trigonometry Constants for Orbital Paths 
+let theta = 0; // Current angle
+// Angle increment on each render
+const dTheta = 2 * Math.PI / 100;
+
 // rendering the scene
 function rendering() {
+  // Rerender every time the page refreshes (pause when on another tab)
   requestAnimationFrame(rendering);
 
-  // Update trackball controls
+  // update trackball controls
   controls.update();
 
-  scene.rotation.x += 0.02;
+  scene.rotation.x += 0.002;
   scene.rotation.y += 0.02;
+
+  //Increment theta, and update sphere coords based off new value        
+  theta += dTheta;
+
+  // Store trig functions for sphere orbits 
+    // MUST BE INSIDE RENDERING FUNCTION OR THETA VALUES ONLY GET SET ONCE
+    const trigs = [
+      {x: Math.cos(theta*1.05), y: Math.sin(theta*1.05), z: Math.cos(theta*1.05), r: 2},
+      {x: Math.cos(theta*0.8), y: Math.sin(theta*0.8), z: Math.sin(theta*0.8), r: 2.25},
+      {x: Math.cos(theta*1.25), y: Math.cos(theta*1.25), z: Math.sin(theta*1.25), r: 2.5},
+      {x: Math.sin(theta*0.6), y: Math.cos(theta*0.6), z: Math.sin(theta*0), r: 2.75}
+  ];
+  
+  // Loop 4 times (for each sphere), updating the position 
+  for (let i=0; i<4; i++) {
+    sphereMeshes[i].position.x = trigs[i]['r'] * trigs[i]['x'];
+    sphereMeshes[i].position.y = trigs[i]['r'] * trigs[i]['y'];
+    sphereMeshes[i].position.z = trigs[i]['r'] * trigs[i]['z'];
+};
 
   renderer.render(scene, camera);
 };
